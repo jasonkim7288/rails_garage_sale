4a7f991dc39b1d9923902c53d52a1953c01756da
jasonkim7288 <jason.kim7288@gmail.com>
2020-06-15 16:13:12 +1000

    [bug_fix] omniauth fail fix - 04

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -5,6 +5,8 @@ class OmniauthController < Devise::RegistrationsController
             sign_in_and_redirect @user
             set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
         else
+            puts "***************************"
+            p @user
             flash[:error] = 'There was a problem signing you in through Facebook. Please register or try signing in later'
             redirect_to new_user_registration_url
         end
